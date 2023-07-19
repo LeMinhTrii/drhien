4612e306ecf083f438055a9309e66d50f825f0c3
le minh tri <leminhtri886@gmail.com>
2023-07-19 10:39:18 +0700

    first commit

--- dest/main.js
+++ dest/main.js
@@ -1,26 +1,27 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    responsiveClass: true,
-    center: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    animateOut: true,
-    animateIn: true,
-    responsive: {
-      0: {
-        items: 1,
-        center: false,
-      },
-      997: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
-    },
-  });
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        center: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        animateOut: true,
+        animateIn: true,
+        responsive: {
+            0: {
+                items: 1,
+                center: false,
+            },
+            997: {
+                items: 1,
+                center: false,
+            },
+            1000: {
+                items: 3,
+            },
+        },
+    });
 });
