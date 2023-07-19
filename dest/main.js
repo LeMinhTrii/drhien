9bc8e4549d6fff94373acc161eb40851acc5fd9d
le minh tri <leminhtri886@gmail.com>
2023-07-19 11:28:55 +0700

    first commit

--- dest/main.js
+++ dest/main.js
@@ -25,3 +25,9 @@ $(document).ready(function() {
         },
     });
 });
+
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
